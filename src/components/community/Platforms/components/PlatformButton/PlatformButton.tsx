f9ed27a6b8ead89bbeb9d647cdf34722564a0ea2
valid <vali.domokos@gmail.com>
2021-06-30 00:26:36 +0200

    improved folder structure

--- src/components/community/Platforms/components/PlatformButton/PlatformButton.tsx
+++ src/components/community/Platforms/components/PlatformButton/PlatformButton.tsx
@@ -1,9 +1,9 @@
 import { Button, useDisclosure } from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
-import LeaveModal from "./LeaveModal"
-import JoinModal from "./JoinModal"
-import platformsContent from "../platformsContent"
-import useIsMember from "./JoinModal/hooks/useIsMember"
+import LeaveModal from "../LeaveModal"
+import JoinModal from "../JoinModal"
+import platformsContent from "../../platformsContent"
+import useIsMember from "./hooks/useIsMember"
 
 type Props = {
   platform: string
