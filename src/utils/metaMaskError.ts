2b621309bf1f70d8db2585f35eda64fc83c20541
valid <vali.domokos@gmail.com>
2021-07-09 14:07:47 +0200

    content improvements & removed cases

--- src/utils/metaMaskError.ts
+++ src/utils/metaMaskError.ts
@@ -6,19 +6,8 @@ const processMetaMaskError = (error: MetaMaskError): ErrorInfo => {
   switch (error.code) {
     case 4001:
       return {
-        title: "Request Rejected",
-        description: "Request got rejected by the user",
-      }
-    case 4100:
-      return {
-        title: "Unauthorized",
-        description:
-          "The requested method and/or account has not been authorized by the user",
-      }
-    case 4200:
-      return {
-        title: "Unsupported Method",
-        description: "MetaMask does not support the requested method",
+        title: "Request rejected",
+        description: "Please try again and confirm the request in MetaMask",
       }
     case 4900:
       return {
@@ -27,68 +16,43 @@ const processMetaMaskError = (error: MetaMaskError): ErrorInfo => {
       }
     case 4901:
       return {
-        title: "Chain Disconnected",
+        title: "Chain disconnected",
         description: "MetaMask is not connected to the requested chain",
       }
-    case -32603:
+    case 4100:
       return {
-        title: "Internal error",
-        description: "Internal JSON-RPC error",
+        title: "Unauthorized",
+        description: "The requested method and/or account has not been authorized",
+      }
+    case 4200:
+      return {
+        title: "Unsupported method",
+        description: "MetaMask does not support the requested method",
       }
     case -32003:
       return {
         title: "Transaction rejected",
-        description: "Transaction creation failed",
+        description: "Transaction creation failed. Please try again!",
+      }
+    case -32603:
+      return {
+        title: "Internal error",
+        description: "If you've set a custom gas price, that may be wrong",
       }
     case -32001:
       return {
         title: "Resource not found",
-        description: "Requested resource not found",
+        description: "Requested resource is not found",
       }
     case -32002:
       return {
         title: "Resource unavailable",
-        description: "Requested resource not available",
-      }
-    case -32600:
-      return {
-        title: "Invalid request",
-        description: "JSON is not a valid request object",
-      }
-    case -32700:
-      return {
-        title: "Parse error",
-        description: "Invalid JSON",
-      }
-    case -32601:
-      return {
-        title: "Method not found",
-        description: "Method does not exist",
-      }
-    case -32602:
-      return {
-        title: "Invalid params",
-        description: "Invalid method parameters",
-      }
-    case -32000:
-      return {
-        title: "Invalid input",
-        description: "Missing or invalid parameters",
-      }
-    case -32004:
-      return {
-        title: "Method not supported",
-        description: "Method is not implemented",
+        description: "Requested resource is not available",
       }
     case -32005:
       return {
         title: "Limit exceeded",
         description: "Request exceeds defined limit",
-      }
-    case -32006:
-      return {
-        title: "JSON-RPC version not supported",
-        description: "Version of JSON-RPC protocol is not supported",
       }
     default:
       console.error(error)
