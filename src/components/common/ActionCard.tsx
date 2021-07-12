481b1213dcb303f16c1024b641662f2c87c41fb7
KovJonas <kovacsjonas99@gmail.com>
2021-07-12 13:01:17 +0200

    Removed accordion functionality from ActionCard

--- src/components/common/ActionCard.tsx
+++ src/components/common/ActionCard.tsx
@@ -32,67 +32,15 @@ const ActionCardContent = ({ title, description, children }: ContentProps) => (
 )
 
 type Props = {
-  withAccordion?: boolean
-  opened?: boolean
   title: string
   description: string | JSX.Element[]
   children: JSX.Element | JSX.Element[]
 }
 
-const ActionCard = ({
-  withAccordion = false,
-  opened,
-  title,
-  description,
-  children,
-}: Props): JSX.Element => {
-  if (!withAccordion) {
-    return (
-      <Card isFullWidthOnMobile p={6}>
-        <ActionCardContent {...{ title, description, children }} />
-      </Card>
-    )
-  }
-
-  return (
-    <>
-      <Card
-        isFullWidthOnMobile
-        display={{ base: "block", md: "none" }}
-        p={6}
-        h="full"
-      >
-        <Accordion defaultIndex={opened && 0} allowMultiple>
-          <AccordionItem border="none">
-            <AccordionButton p="0" _hover={{ bg: "none" }}>
-              <Stack
-                w="full"
-                justifyContent="space-between"
-                justifyItems="center"
-                direction="row"
-              >
-                <Heading size="sm">{title}</Heading>
-                <AccordionIcon />
-              </Stack>
-            </AccordionButton>
-
-            <AccordionPanel p="0">
-              <Text pt="2" mb="6" fontWeight="medium">
-                {description}
-              </Text>
-              <Wrap spacing="2" justify="flex-end" mt="auto" shouldWrapChildren>
-                {children}
-              </Wrap>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Card>
-
-      <Card display={{ base: "none", md: "flex" }} p={6} h="full">
-        <ActionCardContent {...{ title, description, children }} />
-      </Card>
-    </>
-  )
-}
+const ActionCard = ({ title, description, children }: Props): JSX.Element => (
+  <Card isFullWidthOnMobile p={6}>
+    <ActionCardContent {...{ title, description, children }} />
+  </Card>
+)
 
 export default ActionCard
