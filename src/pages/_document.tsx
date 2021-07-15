c67ba770583b7651a2806e2cab97e5c24958da3a
Kovács Jónás <kovacsjonas99@gmail.com>
2021-07-15 15:32:46 +0200

    Removed color mode script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
-import { ColorModeScript } from "@chakra-ui/react"
-import theme from "../theme"
 
 class MyDocument extends Document {
   render(): JSX.Element {
@@ -21,7 +19,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
