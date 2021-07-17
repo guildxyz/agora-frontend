b4d9bdfe7237549c71502348d2f687de76e343c6
Kovács Jónás <kovacsjonas99@gmail.com>
2021-07-17 15:45:13 +0200

    Fixed the colors in the Tag component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -2,6 +2,7 @@ import Button from "./button"
 import Alert from "./alert"
 import Modal from "./modal"
 import Tooltip from "./tooltip"
+import Tag from "./tag"
 
-const components = { Button, Alert, Modal, Tooltip }
+const components = { Button, Alert, Modal, Tooltip, Tag }
 export default components
