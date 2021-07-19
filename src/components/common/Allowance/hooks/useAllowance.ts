4e5f0832673bbde32a9ad9cf3121e685a4530e0d
valid <vali.domokos@gmail.com>
2021-07-19 16:22:21 +0200

    removed unused files

--- src/components/common/Allowance/hooks/useAllowance.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react"
-
-const AllowanceContext = createContext(null)
-
-const useAllowance = () => useContext(AllowanceContext)
-
-export { useAllowance, AllowanceContext }
--- src/components/common/Allowance/index.ts
+++
@@ -1,5 +0,0 @@
-import AllowanceModal from "./AllowanceModal"
-import AllowanceModalBody from "./AllowanceModalBody"
-import AllowanceModalFooter from "./AllowanceModalFooter"
-
-export { AllowanceModal, AllowanceModalBody, AllowanceModalFooter }
