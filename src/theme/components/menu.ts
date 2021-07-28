fd0d27dac23bf9a8ebeb0945a3447a038d83097a
valid <vali.domokos@gmail.com>
2021-07-28 19:29:21 +0200

    comment hotfix

--- src/theme/components/menu.ts
+++ src/theme/components/menu.ts
@@ -2,7 +2,7 @@ const styles = {
   parts: ["list"],
   baseStyle: {
     list: {
-      // we can't add data attributes to the Modal component so we have
+      // we can't add data attributes to the Menu component so we have
       // to prevent the focus-visible polyfill from removing shadow on
       // focus by overriding it's style with the default box-shadow
       ":focus:not([data-focus-visible-added])": {
