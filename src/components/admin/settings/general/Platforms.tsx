4cb1f8c6987e77acbb6e827083457f0c18a36386
KovJonas <kovacsjonas99@gmail.com>
2021-08-05 15:30:52 +0200

    Platforms section & Links section fix

--- src/components/admin/settings/general/Platforms.tsx
+++ src/components/admin/settings/general/Platforms.tsx
@@ -1,4 +1,16 @@
-import { Text } from "@chakra-ui/react"
+import {
+  Divider,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Switch,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
 import Section from "components/admin/Section"
 
 const Platforms = (): JSX.Element => (
@@ -7,7 +19,57 @@ const Platforms = (): JSX.Element => (
     description="Chat platforms your community will be available on"
     cardType
   >
-    <Text>Content</Text>
+    <VStack>
+      <Grid width="full" templateColumns="auto 100%" gap={12}>
+        <GridItem colSpan={1}>
+          <FormControl display="flex" height="full" alignItems="center">
+            <Switch mr={4} />
+            <FormLabel margin={0}>Discord</FormLabel>
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <FormControl>
+            <InputGroup>
+              <InputLeftAddon>Server ID</InputLeftAddon>
+              <Input width={64} />
+            </InputGroup>
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <FormControl display="flex" height="full" alignItems="center">
+            <Switch mr={4} />
+            <FormLabel margin={0}>Telegram</FormLabel>
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <Text color="gray.500">
+            You'll have to set the group IDs for every level
+          </Text>
+        </GridItem>
+
+        <GridItem colSpan={2}>
+          <Divider />
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <FormControl display="flex" height="full" alignItems="center">
+            <Switch mr={4} />
+            <FormLabel width="max-content" margin={0}>
+              Bridge platforms
+            </FormLabel>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Text color="gray.500">
+            All messages will be forwarded to every platform, so the community is
+            unified
+          </Text>
+        </GridItem>
+      </Grid>
+    </VStack>
   </Section>
 )
 
