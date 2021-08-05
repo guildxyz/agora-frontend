9b512ea195547c0054acceedab01ef31642d0281
KovJonas <kovacsjonas99@gmail.com>
2021-08-05 20:41:47 +0200

    Appearance section

--- src/components/admin/settings/appearance/Appearance.tsx
+++ src/components/admin/settings/appearance/Appearance.tsx
@@ -1,4 +1,13 @@
-import { Text } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
 import Section from "components/admin/Section"
 
 const Appearance = (): JSX.Element => (
@@ -7,7 +16,29 @@ const Appearance = (): JSX.Element => (
     description="Make your community page as coherent with your brand as you can, so the members will feel familiar"
     cardType
   >
-    <Text>Content</Text>
+    <VStack spacing={2} alignItems="start">
+      <Text>Main color</Text>
+      <HStack spacing={4}>
+        <Box w={10} h={10} rounded="full" bgColor="indigo.500" />
+        <InputGroup maxWidth={60}>
+          <Input />
+          <InputRightAddon px={0}>
+            <Button
+              width="full"
+              height="full"
+              rounded="none"
+              fontSize="sm"
+              fontWeight="normal"
+            >
+              Pick another
+            </Button>
+          </InputRightAddon>
+        </InputGroup>
+      </HStack>
+      <Text color="gray.600" fontSize="sm">
+        Generated from your image
+      </Text>
+    </VStack>
   </Section>
 )
 
