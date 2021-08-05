9a022aabfeea1859bb7d94b905dde214e9cd20be
KovJonas <kovacsjonas99@gmail.com>
2021-08-05 15:11:14 +0200

    Links section

--- src/components/admin/settings/general/Links.tsx
+++ src/components/admin/settings/general/Links.tsx
@@ -1,5 +1,14 @@
-import { Text } from "@chakra-ui/react"
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Icon,
+  Input,
+} from "@chakra-ui/react"
 import Section from "components/admin/Section"
+import { Plus } from "phosphor-react"
 
 const Links = (): JSX.Element => (
   <Section
@@ -7,7 +16,39 @@ const Links = (): JSX.Element => (
     description="Links to other platforms to show on your communitiy’s page"
     cardType
   >
-    <Text>Content</Text>
+    <Grid templateColumns="repeat(2, 1fr)" gap={12}>
+      <GridItem>
+        <FormControl id="github">
+          <FormLabel>Github</FormLabel>
+          <Input />
+        </FormControl>
+      </GridItem>
+
+      <GridItem>
+        <FormControl id="medium">
+          <FormLabel>Medium</FormLabel>
+          <Input />
+        </FormControl>
+      </GridItem>
+
+      <GridItem>
+        <FormControl id="twitter">
+          <FormLabel>Twitter</FormLabel>
+          <Input />
+        </FormControl>
+      </GridItem>
+
+      <GridItem>
+        <FormControl id="reddit">
+          <FormLabel>Reddit</FormLabel>
+          <Input />
+        </FormControl>
+      </GridItem>
+    </Grid>
+
+    <Button mt={8} width="max-content" variant="ghost" leftIcon={<Icon as={Plus} />}>
+      Add more
+    </Button>
   </Section>
 )
 
