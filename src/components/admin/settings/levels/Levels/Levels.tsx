279f9c9c708f1ba7299517e73a57cc7cd558adf7
KovJonas <kovacsjonas99@gmail.com>
2021-08-06 09:07:08 +0200

    Add level button

--- src/components/admin/settings/levels/Levels/Levels.tsx
+++ src/components/admin/settings/levels/Levels/Levels.tsx
@@ -1,5 +1,6 @@
-import { VStack } from "@chakra-ui/react"
+import { Button, Divider, HStack, Icon, VStack } from "@chakra-ui/react"
 import Section from "components/admin/Section"
+import { Plus } from "phosphor-react"
 import AddLevel from "./components/AddLevel"
 
 const Levels = (): JSX.Element => (
@@ -7,9 +8,19 @@ const Levels = (): JSX.Element => (
     title="Levels"
     description="Ordered from the most accessible to the most VIP one. Each one gives access to the lower levels too"
   >
-    <VStack width="full" gap={12}>
-      <AddLevel />
-    </VStack>
+    <>
+      <VStack width="full" gap={12}>
+        <AddLevel />
+      </VStack>
+
+      <HStack width="full" spacing={2}>
+        <Divider borderBottomWidth={2} borderColor="gray.300" />
+        <Button width={60} variant="ghost" leftIcon={<Icon as={Plus} />}>
+          Add level
+        </Button>
+        <Divider borderBottomWidth={2} borderColor="gray.300" />
+      </HStack>
+    </>
   </Section>
 )
 
