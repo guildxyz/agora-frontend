64295c2708b36463cac4d61ac323e485ca0a2619
Balázs Csete <csetebalu@gmail.com>
2021-08-11 16:35:52 +0200

    added back provider in community index

--- src/pages/[community]/index.tsx
+++ src/pages/[community]/index.tsx
@@ -2,6 +2,7 @@ import { Icon, Stack, Text } from "@chakra-ui/react"
 import Card from "components/common/Card"
 import Layout from "components/common/Layout"
 import ActionCard from "components/[community]/common/ActionCard"
+import { CommunityProvider } from "components/[community]/common/Context"
 import Pagination from "components/[community]/common/Pagination"
 import { Info } from "phosphor-react"
 import type { Community } from "temporaryData/communities"
@@ -11,28 +12,30 @@ type Props = {
 }
 
 const CommunityPage = ({ communityData }: Props): JSX.Element => (
-  <Layout title={communityData.name} imageUrl={communityData.imageUrl}>
-    <Stack spacing={{ base: 7, xl: 9 }}>
-      <Pagination />
-      <Stack spacing={{ base: 7 }}>
-        <ActionCard
-          title="About"
-          description={communityData.description || "No description"}
-        />
-        <Card p="6" isFullWidthOnMobile>
-          <Text
-            fontWeight="medium"
-            colorScheme="gray"
-            display="flex"
-            alignItems="center"
-          >
-            <Icon as={Info} mr="2" />
-            More info coming soon
-          </Text>
-        </Card>
+  <CommunityProvider data={communityData}>
+    <Layout title={communityData.name} imageUrl={communityData.imageUrl}>
+      <Stack spacing={{ base: 7, xl: 9 }}>
+        <Pagination />
+        <Stack spacing={{ base: 7 }}>
+          <ActionCard
+            title="About"
+            description={communityData.description || "No description"}
+          />
+          <Card p="6" isFullWidthOnMobile>
+            <Text
+              fontWeight="medium"
+              colorScheme="gray"
+              display="flex"
+              alignItems="center"
+            >
+              <Icon as={Info} mr="2" />
+              More info coming soon
+            </Text>
+          </Card>
+        </Stack>
       </Stack>
-    </Stack>
-  </Layout>
+    </Layout>
+  </CommunityProvider>
 )
 
 export {
