ba9cff8e094bd9522c83357600bd68fcb9488b39
Balázs Csete <csetebalu@gmail.com>
2021-08-11 13:20:26 +0200

    useMemberCount refactor
    Handling the logic of previous useMemo in the fetcher

--- src/components/[community]/hooks/useMemberCount.ts
+++ src/components/[community]/hooks/useMemberCount.ts
@@ -1,24 +1,29 @@
 import useKeepSWRDataLiveAsBlocksArrive from "hooks/useKeepSWRDataLiveAsBlocksArrive"
-import { useMemo } from "react"
 import useSWR from "swr"
 import { Level } from "temporaryData/types"
-
-type MemberCountResponse = {
-  id: number
-  membersCount: number
-}
 
 type MemberCount = {
   sum?: number
   [levelId: string]: number
 }
 
-const getMemberCount = (_: string, id: number): Promise<MemberCountResponse[]> =>
-  fetch(`https://api.agora.space/community/getMemberCount/${id}`).then((response) =>
-    response.ok
-      ? response.json()
-      : Promise.reject(new Error(`Unable to fetch member count of community ${id}`))
-  )
+const getMemberCount = (_: string, id: number): Promise<MemberCount> =>
+  fetch(`https://api.agora.space/community/getMemberCount/${id}`)
+    .then((response) =>
+      response.ok
+        ? response.json()
+        : Promise.reject(
+            new Error(`Unable to fetch member count of community ${id}`)
+          )
+    )
+    .then((data) => {
+      const levelsData: MemberCount = { sum: 0 }
+      data.forEach(({ id: levelId, membersCount }) => {
+        levelsData[levelId] = membersCount
+        levelsData.sum += membersCount
+      })
+      return levelsData
+    })
 
 const useMemberCount = (communityId: number, initialLevels: Level[]) => {
   // the mocked communities from tokens.json have negative ids
@@ -28,25 +33,18 @@ const useMemberCount = (communityId: number, initialLevels: Level[]) => {
     shouldFetch ? ["membercount", communityId] : null,
     getMemberCount,
     {
-      initialData: initialLevels.map(({ id, membersCount }) => ({
-        id,
-        membersCount,
-      })),
+      initialData: {
+        sum: initialLevels.reduce((acc, curr) => acc + curr.membersCount, 0),
+        ...Object.fromEntries(
+          initialLevels.map(({ id, membersCount }) => [id, membersCount])
+        ),
+      },
     }
   )
 
-  const memberCountData = useMemo(() => {
-    const levelsData: MemberCount = {}
-    data.forEach(({ id, membersCount }) => {
-      levelsData[id] = membersCount
-      levelsData.sum = (levelsData.sum ?? 0) + membersCount
-    })
-    return levelsData
-  }, [data])
-
   useKeepSWRDataLiveAsBlocksArrive(mutate)
 
-  return memberCountData
+  return data
 }
 
 export default useMemberCount
