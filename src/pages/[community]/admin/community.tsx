5867ea10e312f4639e52f4e3f060685d74380036
KovJonas <kovacsjonas99@gmail.com>
2021-08-16 16:45:25 +0200

    Add levels page - basic error handling

--- src/pages/[community]/admin/community.tsx
+++ src/pages/[community]/admin/community.tsx
@@ -5,6 +5,7 @@ import {
   Box,
   Button,
   Stack,
+  useToast,
   VStack,
 } from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
@@ -13,6 +14,7 @@ import Platforms from "components/admin/community/Platforms"
 import clearUndefinedData from "components/admin/utils/clearUndefinedData"
 import Layout from "components/common/Layout"
 import Pagination from "components/[community]/common/Pagination"
+import usePersonalSign from "components/[community]/community/Platforms/components/JoinModal/hooks/usePersonalSign"
 import React from "react"
 import { FormProvider, useForm } from "react-hook-form"
 import { Community } from "temporaryData/types"
@@ -26,8 +28,12 @@ const convertMonthsToMs = (months: number) =>
   Math.round(months / 3.8026486208174e-10)
 
 const AdminCommunityPage = ({ communityData }: Props): JSX.Element => {
+  const toast = useToast()
+
   const { chainId } = useWeb3React()
   const methods = useForm({ mode: "all" })
+
+  const sign = usePersonalSign()
 
   const onSubmit = (data) => {
     const editedData = { ...data }
@@ -52,15 +58,46 @@ const AdminCommunityPage = ({ communityData }: Props): JSX.Element => {
 
     const finalData = clearUndefinedData(editedData)
 
-    fetch(`${process.env.NEXT_PUBLIC_API}/community/levels/${communityData.id}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(finalData),
-    })
-      .then((response) => {
-        console.log(response)
-      })
-      .catch(console.error)
+    // Signing the message, and sending the data to the API
+    sign("Please sign this message to verify your address").then(
+      (addressSignedMessage) => {
+        fetch(
+          `${process.env.NEXT_PUBLIC_API}/community/levels/${communityData.id}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ...finalData, addressSignedMessage }),
+          }
+        )
+          .then((response) => {
+            if (response.status !== 201) {
+              toast({
+                title: "Error",
+                description:
+                  "An error occurred while adding levels to your community",
+                status: "error",
+                duration: 4000,
+              })
+              return
+            }
+
+            toast({
+              title: "Success!",
+              description: "Level(s) added!",
+              status: "success",
+              duration: 2000,
+            })
+          })
+          .catch(() => {
+            toast({
+              title: "Error",
+              description: "Server error",
+              status: "error",
+              duration: 4000,
+            })
+          })
+      }
+    )
   }
 
   if (!chainId) {
