94c6f26368969a7ddd76e76977a7bf6cc3b8a8ec
KovJonas <kovacsjonas99@gmail.com>
2021-08-18 09:14:33 +0200

    useSubmitLevelsData hook & community levels update

---
+++ src/components/admin/hooks/useSubmitLevelsData.ts
@@ -0,0 +1,120 @@
+import { useToast } from "@chakra-ui/react"
+import usePersonalSign from "components/[community]/community/Platforms/components/JoinModal/hooks/usePersonalSign"
+import clearUndefinedData from "../utils/clearUndefinedData"
+
+const useSubmitLevelsData = (method: "POST" | "PATCH", id = null) => {
+  const fetchUrl = `${process.env.NEXT_PUBLIC_API}/community/${
+    method === "POST" ? `levels/${id}` : "level"
+  }`
+  const toast = useToast()
+  const sign = usePersonalSign()
+
+  // Helper method for converting month(s) to ms
+  const convertMonthsToMs = (months: number) =>
+    Math.round(months / 3.8026486208174e-10)
+
+  const onSubmit = (data: any) => {
+    const editedData = { ...data }
+
+    // Won't send these to the backend
+    delete editedData.isDCEnabled
+    delete editedData.isTGEnabled
+
+    // Converting timeLock to ms for every level
+    editedData.levels = editedData.levels?.map((level) => {
+      const timeLock = level.stakeTimelockMs
+
+      if (!timeLock) {
+        return clearUndefinedData(level)
+      }
+
+      return {
+        ...clearUndefinedData(level),
+        stakeTimelockMs: convertMonthsToMs(timeLock),
+      }
+    })
+
+    const finalData: any = clearUndefinedData(editedData)
+
+    // Signing the message, and sending the data to the API
+    sign("Please sign this message to verify your address").then(
+      (addressSignedMessage) => {
+        if (method === "POST") {
+          fetch(fetchUrl, {
+            method,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ...finalData, addressSignedMessage }),
+          })
+            .then((response) => {
+              if (response.status !== 200 && response.status !== 201) {
+                toast({
+                  title: "Error",
+                  description:
+                    "An error occurred while adding levels to your community",
+                  status: "error",
+                  duration: 4000,
+                })
+                return
+              }
+
+              toast({
+                title: "Success!",
+                description: "Level(s) added!",
+                status: "success",
+                duration: 2000,
+              })
+            })
+            .catch(() => {
+              toast({
+                title: "Error",
+                description: "Server error",
+                status: "error",
+                duration: 4000,
+              })
+            })
+          return
+        }
+
+        // On PATCH - sending a PATCH request for every level
+        const levelsToUpdate = [...finalData.levels].map((level) =>
+          fetch(`${fetchUrl}/${level.id}`, {
+            method,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ...level, addressSignedMessage }),
+          })
+        )
+        Promise.all(levelsToUpdate)
+          .then((responses) => {
+            if (responses.find((res) => res.status !== 200 && res.status !== 201)) {
+              toast({
+                title: "Error",
+                description:
+                  "An error occurred while editing the levels of your community",
+                status: "error",
+                duration: 4000,
+              })
+              return
+            }
+            toast({
+              title: "Success!",
+              description: "Level(s) updated!",
+              status: "success",
+              duration: 2000,
+            })
+          })
+          .catch(() => {
+            toast({
+              title: "Error",
+              description: "Server error",
+              status: "error",
+              duration: 4000,
+            })
+          })
+      }
+    )
+  }
+
+  return onSubmit
+}
+
+export default useSubmitLevelsData
--- src/pages/[community]/admin/community.tsx
+++ src/pages/[community]/admin/community.tsx
@@ -1,12 +1,11 @@
-import { Box, Button, Stack, useToast, VStack } from "@chakra-ui/react"
+import { Box, Button, Stack, VStack } from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
 import NotConnectedError from "components/admin/common/NotConnectedError"
 import Levels from "components/admin/community/Levels"
 import Platforms from "components/admin/community/Platforms"
-import clearUndefinedData from "components/admin/utils/clearUndefinedData"
+import useSubmitLevelsData from "components/admin/hooks/useSubmitLevelsData"
 import Layout from "components/common/Layout"
 import Pagination from "components/[community]/common/Pagination"
-import usePersonalSign from "components/[community]/community/Platforms/components/JoinModal/hooks/usePersonalSign"
 import useColorPalette from "components/[community]/hooks/useColorPalette"
 import { useRouter } from "next/router"
 import React, { useEffect } from "react"
@@ -17,85 +16,33 @@ type Props = {
   communityData: Community
 }
 
-// Helper method for converting month(s) to ms
-const convertMonthsToMs = (months: number) =>
-  Math.round(months / 3.8026486208174e-10)
-
 const AdminCommunityPage = ({ communityData }: Props): JSX.Element => {
   const router = useRouter()
-  const toast = useToast()
   const { chainId, account } = useWeb3React()
   const generatedColors = useColorPalette(
     "chakra-colors-primary",
     communityData.themeColor || "#71717a"
   )
-  const methods = useForm({ mode: "all" })
-  const sign = usePersonalSign()
+  const methods = useForm({
+    mode: "all",
+    defaultValues: {
+      levels: communityData.levels.map((level) => ({
+        id: level.id,
+        name: level.name,
+        image: level.imageUrl,
+        description: level.description,
+        requirementType: level.requirementType,
+        requirement: level.requirement,
+        stakeTimelockMs: level.stakeTimelockMs, // TODO: convert it to months
+        telegramGroupId: level.telegramGroupId,
+      })),
+    },
+  })
 
-  const onSubmit = (data) => {
-    const editedData = { ...data }
-
-    // Won't send these to the backend
-    delete editedData.isDCEnabled
-    delete editedData.isTGEnabled
-
-    // Converting timeLock to ms for every level
-    editedData.levels = editedData.levels?.map((level) => {
-      const timeLock = level.stakeTimelockMs
-
-      if (!timeLock) {
-        return clearUndefinedData(level)
-      }
-
-      return {
-        ...clearUndefinedData(level),
-        stakeTimelockMs: convertMonthsToMs(timeLock),
-      }
-    })
-
-    const finalData = clearUndefinedData(editedData)
-
-    // Signing the message, and sending the data to the API
-    sign("Please sign this message to verify your address").then(
-      (addressSignedMessage) => {
-        fetch(
-          `${process.env.NEXT_PUBLIC_API}/community/levels/${communityData.id}`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ ...finalData, addressSignedMessage }),
-          }
-        )
-          .then((response) => {
-            if (response.status !== 201) {
-              toast({
-                title: "Error",
-                description:
-                  "An error occurred while adding levels to your community",
-                status: "error",
-                duration: 4000,
-              })
-              return
-            }
-
-            toast({
-              title: "Success!",
-              description: "Level(s) added!",
-              status: "success",
-              duration: 2000,
-            })
-          })
-          .catch(() => {
-            toast({
-              title: "Error",
-              description: "Server error",
-              status: "error",
-              duration: 4000,
-            })
-          })
-      }
-    )
-  }
+  const onSubmit = useSubmitLevelsData(
+    communityData.levels?.length > 0 ? "PATCH" : "POST",
+    communityData.id
+  )
 
   useEffect(() => {
     if (account && account.toLowerCase() !== communityData.owner.address) {
