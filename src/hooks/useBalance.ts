b9048dfd21b70fe9a0d4a7543a6d914fb0b1eae6
valid <vali.domokos@gmail.com>
2021-08-19 02:35:43 +0200

    WIP

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -15,7 +15,9 @@ const getBalance = async (
   decimals: number
 ): Promise<number> =>
   tokenContract &&
-  tokenContract.balanceOf(address).then((balance) => formatUnits(balance, decimals))
+  tokenContract
+    .balanceOf(address)
+    .then((balance) => parseInt(formatUnits(balance, decimals)))
 
 const useBalance = (token: Token): number => {
   const { library, chainId, account } = useWeb3React<Web3Provider>()
--- src/hooks/useMutagenNfts.ts
+++ src/hooks/useMutagenNfts.ts
@@ -1,44 +1,65 @@
-import { Contract } from "@ethersproject/contracts"
+import { aggregate } from "@makerdao/multicall"
 import { useWeb3React } from "@web3-react/core"
-import mutagenABI from "constants/mutagenABI.json"
 import useSWR from "swr"
 import { RequirementType, Token } from "temporaryData/types"
 import useBalance from "./useBalance"
-import useContract from "./useContract"
 import useKeepSWRDataLiveAsBlocksArrive from "./useKeepSWRDataLiveAsBlocksArrive"
+
+const config = { preset: "goerli" }
 
 const getMutagenNfts = async (
   _: string,
   amount: number,
-  contract: Contract,
+  mutagenAddress: string,
   account: string
-) =>
-  Promise.all(
-    [...Array(amount)].map((_, i) =>
-      (async () => {
-        const tokenId = await contract.tokenOfOwnerByIndex(account, i)
-        // eslint-disable-next-line no-bitwise
-        const tokenType = tokenId & 3
-        // const url = await contract.tokenURI(tokenId)
-        // const data = await fetch(url, { mode: "no-cors" })
-        return tokenType
-      })()
-    )
-  )
+) => {
+  const idRequests = [...Array(amount)].map((_, i) => ({
+    target: mutagenAddress,
+    call: ["tokenOfOwnerByIndex(address,uint256)(uint256)", account, i],
+    returns: [[i, (val) => parseInt(val)]],
+  }))
+  console.log(idRequests)
+
+  const {
+    results: { transformedIds },
+  } = await aggregate(idRequests, config)
+  console.log(transformedIds)
+  const ids = Object.values(transformedIds)
+
+  const dataRequests = ids.map((id) => ({
+    target: mutagenAddress,
+    call: ["unpackPrintId(uint256)(uint8,uint256,uint16)", id],
+    returns: [[id], [], []],
+  }))
+  const {
+    results: { transformedData },
+  } = await aggregate(dataRequests, config)
+  const correctIdsArray = Object.entries(transformedData).slice(0, -1)
+  const data = correctIdsArray.reduce((arr, [tokenId, genesisIdx]) => {
+    // eslint-disable-next-line no-bitwise
+    const type = tokenId as any & 3
+    if (type === 0) {
+      arr.push(`Genesis_${genesisIdx}`)
+    } else if (type === 1) {
+      arr.push(`Print_${genesisIdx}`)
+    }
+    return arr
+  }, [])
+  return data
+}
 
 const useMutagenNfts = (requirementType: RequirementType, token: Token) => {
   const { account } = useWeb3React()
-  const amount: any = useBalance(token)
-  const contract = useContract(token?.address, mutagenABI)
+  const amount = useBalance(token)
 
-  const shouldFetch = requirementType === "NFT_HOLD" && !!contract && amount > 0
+  const shouldFetch = requirementType === "NFT_HOLD" && !!token && amount > 0
 
   /**
    * TODO: deduping doesn't work for some reason. getMutagenNfts get's called 4 times
-   * in a row even tho the parameters didn't change
+   * in a row even though the parameters haven't changed
    */
   const { data, mutate } = useSWR(
-    shouldFetch ? ["mutagen", parseInt(amount, 10), contract, account] : null,
+    shouldFetch ? ["mutagen", amount, token.address, account] : null,
     getMutagenNfts
   )
 
