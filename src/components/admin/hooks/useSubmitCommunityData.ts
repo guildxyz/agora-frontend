84d3c26db37f5e6fdc5e64479b112306872925c6
Balázs Csete <csetebalu@gmail.com>
2021-08-25 18:48:05 +0200

    useSubmitCommunityData with state machine

--- src/components/admin/hooks/useSubmitCommunityData.ts
+++ src/components/admin/hooks/useSubmitCommunityData.ts
@@ -1,12 +1,106 @@
+import { useMachine } from "@xstate/react"
 import usePersonalSign from "components/[community]/community/Platforms/components/JoinModal/hooks/usePersonalSign"
 import useToast from "hooks/useToast"
 import { useRouter } from "next/router"
-import { useState } from "react"
-import clearUndefinedData from "../utils/clearUndefinedData"
+import { assign, createMachine, DoneInvokeEvent, EventObject } from "xstate"
+import type { ApiError } from "./useShowErrorToast"
 import useShowErrorToast from "./useShowErrorToast"
 
-const useSubmitCommunityData = (method: "POST" | "PATCH", id = null) => {
-  const [loading, setLoading] = useState(false)
+type ContextType = any
+
+// Successful data-flow events
+interface InitialEvent<FormDataType> extends EventObject {
+  data: FormDataType
+}
+type SignEvent<FormDataType> = DoneInvokeEvent<
+  FormDataType & {
+    addressSignedMessage: string
+    urlName: string // So we can do data.urlName to redirect
+  }
+>
+
+// Error events
+type SignError = DoneInvokeEvent<Error>
+type FetchError = DoneInvokeEvent<Response>
+type APIError = DoneInvokeEvent<{ errors: ApiError[] }>
+type ErrorEvent = SignError | FetchError | APIError
+
+const submitCommunityDataMachine = <FormDataType>() =>
+  createMachine<
+    ContextType,
+    InitialEvent<FormDataType> | SignEvent<FormDataType> | ErrorEvent
+  >(
+    {
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            SIGN: "sign",
+          },
+        },
+        sign: {
+          invoke: {
+            src: "sign",
+            onDone: "fetch",
+            onError: "error",
+          },
+        },
+        fetch: {
+          entry: "saveUrlName",
+          invoke: {
+            src: "fetch",
+            onDone: [
+              {
+                target: "success",
+                cond: "fetchSuccessful",
+              },
+              {
+                target: "parseError",
+                cond: "fetchFailed",
+              },
+            ],
+            onError: "error",
+          },
+        },
+        success: {
+          entry: ["showSuccessToast", "redirect"],
+        },
+        parseError: {
+          invoke: {
+            src: "parseError",
+            onDone: "error",
+            onError: "error",
+          },
+        },
+        error: {
+          entry: "showErrorToast",
+          on: {
+            SIGN: "sign",
+          },
+        },
+      },
+    },
+    {
+      services: {
+        parseError: (_, event: FetchError) =>
+          event.data.json().catch(() => Promise.reject(new Error("Network error"))),
+      },
+      guards: {
+        fetchSuccessful: (_context, event: FetchError) => !!event.data.ok,
+        fetchFailed: (_context, event: FetchError) => !event.data.ok,
+      },
+      actions: {
+        saveUrlName: assign((_, { data: { urlName } }: SignEvent<FormDataType>) => ({
+          urlName,
+        })),
+      },
+    }
+  )
+
+const useSubmitCommunityData = <FormDataType>(
+  method: "POST" | "PATCH",
+  id = null
+) => {
   const fetchUrl =
     method === "PATCH"
       ? `${process.env.NEXT_PUBLIC_API}/community/${id}`
@@ -15,67 +109,58 @@ const useSubmitCommunityData = (method: "POST" | "PATCH", id = null) => {
   const toast = useToast()
   const showErrorToast = useShowErrorToast()
   const sign = usePersonalSign()
-
-  const onSubmit = (data: any) => {
-    setLoading(true)
-
-    sign("Please sign this message to verify your address")
-      .then((addressSignedMessage) => {
-        const finalData = clearUndefinedData(data)
-
+  const [state, send] = useMachine(submitCommunityDataMachine<FormDataType>(), {
+    services: {
+      sign: async (_, { data }: InitialEvent<FormDataType>) => {
+        const addressSignedMessage = await sign(
+          "Please sign this message to verify your address"
+        ).catch(() =>
+          Promise.reject(
+            new Error("You must sign the message to verify your address!")
+          )
+        )
+        return { ...data, addressSignedMessage }
+      },
+      fetch: (_context, { data }: SignEvent<FormDataType>) =>
         fetch(fetchUrl, {
           method,
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ ...finalData, addressSignedMessage }),
-        })
-          .then((response) => {
-            setLoading(false)
-
-            if (!response.ok) {
-              response.json().then((json) => showErrorToast(json.errors))
-              return
-            }
-
-            toast({
-              title: "Success!",
-              description:
-                method === "POST"
-                  ? "Community added! You're being redirected to it's page"
-                  : "Community updated! It might take up to 10 sec for the page to update. If it's showing old data, try to refresh it in a few seconds.",
-              status: "success",
-              duration: 2000,
-            })
-
-            if (method === "PATCH") {
-              fetch(`/api/preview?urlName=${finalData.urlName}`)
+          body: JSON.stringify(data),
+        }),
+    },
+    actions: {
+      showErrorToast: (_context, { data: error }: SignError | APIError) => {
+        if (error instanceof Error) showErrorToast(error.message)
+        else showErrorToast(error.errors)
+      },
+      showSuccessToast: () =>
+        toast({
+          title: "Success!",
+          description:
+            method === "POST"
+              ? "Community added! You're being redirected to it's page"
+              : "Community updated! It might take up to 10 sec for the page to update. If it's showing old data, try to refresh it in a few seconds.",
+          status: "success",
+          duration: 2000,
+        }),
+      redirect:
+        method === "PATCH"
+          ? ({ urlName }) =>
+              fetch(`/api/preview?urlName=${urlName}`)
                 .then((res) => res.json())
                 .then((cookies: string[]) => {
                   cookies.forEach((cookie: string) => {
                     document.cookie = cookie
                   })
-
-                  router.push(`/${finalData.urlName}`)
+                  router.push(`/${urlName}`)
                 })
-            }
+          : ({ urlName }) => setTimeout(() => router.push(`/${urlName}`), 2000),
+    },
+  })
 
-            if (method === "POST") {
-              setTimeout(() => {
-                router.push(`/${finalData.urlName}`)
-              }, 2000)
-            }
-          })
-          .catch(() => {
-            setLoading(false)
-            showErrorToast("Server error")
-          })
-      })
-      .catch(() => {
-        setLoading(false)
-        showErrorToast("You must sign the message to verify your address!")
-      })
-  }
+  const onSubmit = (data: FormDataType) => send("SIGN", { data })
 
-  return { onSubmit, loading }
+  return { onSubmit, loading: ["sign", "fetch"].some(state.matches) }
 }
 
 export default useSubmitCommunityData
