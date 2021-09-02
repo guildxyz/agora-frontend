1e98434b9f706ab66097cf76f9ada182f90f73f9
Balázs Csete <csetebalu@gmail.com>
2021-09-02 13:19:30 +0200

    fetching timelocks with new method

--- src/components/[community]/community/Staked/hooks/useStaked.ts
+++ src/components/[community]/community/Staked/hooks/useStaked.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from "@ethersproject/bignumber"
 import { Contract } from "@ethersproject/contracts"
 import { formatEther } from "@ethersproject/units"
 import { useWeb3React } from "@web3-react/core"
@@ -12,53 +13,47 @@ type StakedType = {
   locked: Array<{
     amount: number
     expires: Date
+    rankId: number
     id: number
   }>
 }
 
-const getTimelocks = async (
+type Timelock = { amount: BigNumber; expires: BigNumber; rankId: BigNumber }
+
+const getTimelocks = (
   _: string,
   contract: Contract,
   account: string
-): Promise<StakedType> => {
-  const getStaked = async (
-    i: number,
-    { unlockedAmount, locked }: StakedType
-  ): Promise<StakedType> => {
-    try {
-      const { amount, expires } = await contract.timelocks(account, i)
-      const expiresNumber = expires.toNumber() * 1000
-      if (expiresNumber < Date.now()) {
-        return await getStaked(i + 1, {
-          unlockedAmount: unlockedAmount + +formatEther(amount),
-          locked,
+): Promise<StakedType> =>
+  contract.getTimelocks(account).then((timelocks) =>
+    timelocks.reduce(
+      (
+        _acc: StakedType,
+        { amount: _amount, expires: _expires, rankId: _rankId }: Timelock,
+        index: number
+      ) => {
+        const expires = new Date(_expires.toNumber() * 1000) // Multiplying by 1000, because JS needs the timestamp in milliseconds format
+        const amount = +formatEther(_amount)
+        const rankId = +_rankId
+        const acc = _acc
+        if (+expires < Date.now()) {
+          acc.unlockedAmount += amount
+          return acc
+        }
+        acc.locked.push({
+          amount,
+          expires,
+          rankId,
+          id: index,
         })
+        return acc
+      },
+      {
+        unlockedAmount: 0,
+        locked: [],
       }
-      return await getStaked(i + 1, {
-        unlockedAmount,
-        locked: [
-          ...locked,
-          {
-            amount: +formatEther(amount),
-            expires: new Date(expiresNumber),
-            id: i,
-          },
-        ],
-      })
-    } catch (_) {
-      console.info(
-        "%cThe logged error is expected, it's needed for fetching the user's timelocks. We can't prevent MetaMask from logging it",
-        "color: gray"
-      )
-      return { unlockedAmount, locked }
-    }
-  }
-
-  return getStaked(0, {
-    unlockedAmount: 0,
-    locked: [],
-  })
-}
+    )
+  )
 
 const useStaked = (): StakedType => {
   const {
