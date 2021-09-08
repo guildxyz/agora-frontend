256c28e34ecebd3500ed91c5aac9ac261aad343f
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2021-09-08 15:04:27 +0200

    Trigger field validation on autofill from token address (#137)

--- src/components/admin/index/UsedToken/hooks/useTokenData.ts
+++ src/components/admin/index/UsedToken/hooks/useTokenData.ts
@@ -1,4 +1,5 @@
 import { Contract } from "@ethersproject/contracts"
+import { Logger } from "@ethersproject/logger"
 import ERC_20_ABI from "constants/erc20abi.json"
 import useContract from "hooks/useContract"
 import useSWR from "swr"
@@ -6,7 +7,15 @@ import useSWR from "swr"
 const getTokenData =
   (contract: Contract) =>
   (_: string): Promise<[string, string]> =>
-    Promise.all([contract.name(), contract.symbol()])
+    Promise.all([contract.name(), contract.symbol()]).catch((error) => {
+      /**
+       * This means, that the error occured because the user is on a wrong chain, if
+       * we were revalidating on this error, it would occur again until the the user
+       * doesn't switch to the correct chain
+       */
+      if (error.code === Logger.errors.CALL_EXCEPTION) return [null, null]
+      throw error
+    })
 
 const useTokenData = (address: string, selectedChain: string) => {
   const shouldFetch = /^0x[A-F0-9]{40}$/i.test(address) && selectedChain.length > 0
@@ -16,14 +25,18 @@ const useTokenData = (address: string, selectedChain: string) => {
     shouldFetch ? ["tokenData", address, selectedChain] : null,
     getTokenData(contract),
     {
-      shouldRetryOnError: false,
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
+      errorRetryInterval: 100,
     }
   )
 
   return {
     ...swrResponse,
+    /**
+     * Doing this instead of using initialData to make sure it fetches when
+     * shouldFetch becomes true
+     */
     data: swrResponse.data ?? [undefined, undefined],
   }
 }
