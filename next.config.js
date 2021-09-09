e849afe7454ddcc50a394a577d9edf03eff774cd
Balázs <csetebalu@gmail.com>
2021-09-09 20:03:17 +0200

    alternative solution for redirect rule

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-// TODO
-// module.exports = {
-//   async redirects() {
-//     return [
-//       {
-//         source: "/:path((?!register$))",
-//         destination: "/:path/info",
-//         permanent: false,
-//       },
-//     ]
-//   },
-// }
+module.exports = {
+  async redirects() {
+    const paths = await fetch(`${process.env.NEXT_PUBLIC_API}/community`).then(
+      (response) => response.json().then((_) => _.map(({ urlName }) => urlName))
+    )
+    const urlNames = paths.join("|")
+    return [
+      {
+        // ^(?!register)(.(?!\/))+$
+        source: `/:path(^${urlNames}|[a-zA-Z0-9]+_token$)`,
+        destination: "/:path/info",
+        permanent: false,
+      },
+    ]
+  },
+}
