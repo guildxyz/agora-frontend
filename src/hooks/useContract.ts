77a5510a9a6f18af2a8bd16d775c27b963b598bd
Balázs <csetebalu@gmail.com>
2021-09-20 09:19:16 +0200

    useContract cleanup

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -4,25 +4,26 @@ import type { Web3Provider } from "@ethersproject/providers"
 import { useWeb3React } from "@web3-react/core"
 import useSWR from "swr"
 
-const createContract =
-  (library: Web3Provider, ABI: ContractInterface) =>
-  async (_: string, address: string, withSigner: boolean, account: string) => {
-    console.log(`createContract called - `, address, account)
-    const contract = new Contract(
-      address,
-      ABI,
-      withSigner ? library.getSigner(account).connectUnchecked() : library
-    )
-    const signerAddress = await contract.signer?.getAddress()
-    return { contract, signerAddress }
-  }
+const createContract = async (
+  _: string,
+  address: string,
+  withSigner: boolean,
+  account: string,
+  library: Web3Provider,
+  ABI: ContractInterface
+) =>
+  new Contract(
+    address,
+    ABI,
+    withSigner ? library.getSigner(account).connectUnchecked() : library
+  )
 
 const useContract = (
   address: string,
   ABI: ContractInterface,
   withSigner = false
 ): Contract => {
-  const { account, chainId, library, active } = useWeb3React<Web3Provider>()
+  const { account, chainId, library } = useWeb3React<Web3Provider>()
 
   const shouldFetch =
     typeof address === "string" &&
@@ -31,18 +32,19 @@ const useContract = (
     account.length > 0 &&
     !!library
 
-  const { data } = useSWR(
-    shouldFetch ? ["contract", address, withSigner, account, chainId, active] : null,
-    createContract(library, ABI),
-    {
-      revalidateOnFocus: false,
-      compare: (left, right) =>
-        left?.contract?.address === right?.contract.address &&
-        left?.signerAddress === right?.signerAddress,
-    }
+  /**
+   * Passing library in the dependency array is fine, its basically a constant
+   * reference, won't mix up the cache keys
+   */
+  const { data: contract } = useSWR(
+    shouldFetch
+      ? ["contract", address, withSigner, account, library, ABI, chainId]
+      : null,
+    createContract,
+    { revalidateOnFocus: false }
   )
 
-  return data?.contract
+  return contract
 }
 
 export default useContract
