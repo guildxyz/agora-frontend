79a920757a75bc2c8d33c675e8802c64522a4f90
Balázs <csetebalu@gmail.com>
2021-09-20 09:23:52 +0200

    useBalance cleanup

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -1,5 +1,4 @@
 import { Contract } from "@ethersproject/contracts"
-import { Web3Provider } from "@ethersproject/providers"
 import { formatUnits } from "@ethersproject/units"
 import { useWeb3React } from "@web3-react/core"
 import ERC20_ABI from "constants/erc20abi.json"
@@ -7,34 +6,31 @@ import useSWR from "swr"
 import type { Token } from "temporaryData/types"
 import useContract from "./useContract"
 
-const getBalance =
-  (contract: Contract) =>
-  async (_: string, address: string, decimals: number): Promise<number> =>
-    // console.log("getBalance called", address, decimals, contract)
-    contract.balanceOf(address).then((balance) => +formatUnits(balance, decimals))
+const getBalance = (
+  _: string,
+  address: string,
+  decimals: number,
+  contract: Contract
+): Promise<number> =>
+  contract.balanceOf(address).then((balance) => +formatUnits(balance, decimals))
 
 const useBalance = (token: Token): number => {
-  const { chainId, account } = useWeb3React<Web3Provider>()
+  const { chainId, account } = useWeb3React()
   const tokenContract = useContract(token?.address, ERC20_ABI)
 
   const shouldFetch =
-    typeof account === "string" && !!token && typeof token.address === "string"
+    typeof account === "string" &&
+    typeof token?.address === "string" &&
+    token.address.length > 0 &&
+    !!tokenContract
 
   const { data } = useSWR(
     shouldFetch
-      ? ["balance", account, token?.decimals, chainId, token?.address]
+      ? ["balance", account, token?.decimals, tokenContract, chainId]
       : null,
-    getBalance(tokenContract),
-    {
-      revalidateOnFocus: false,
-      revalidateOnMount: false,
-      dedupingInterval: 5000,
-      refreshInterval: 10_000,
-      // onSuccess: () => console.log("balance fetched", token.symbol),
-    }
+    getBalance,
+    { revalidateOnFocus: false, refreshInterval: 10_000 }
   )
-
-  // useKeepSWRDataLiveAsBlocksArrive(mutate)
 
   return data
 }
